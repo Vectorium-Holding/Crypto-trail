7eabe853763bdd94f6a10ac12cc49550f8d2373f
Arman <fakidarman@gmail.com>
2025-02-19 00:27:33 +0600

    Section7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Section5 from "@/components/Section5";
+import Section7 from "@/components/Section7";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="bg-[#1b1c1d] flex flex-col items-center justify-center">
       <Section5/>
-      {/* <Section7 /> */}
+      <Section7 />
     </div>
   );
 }
--- src/components/Section7.tsx
+++ src/components/Section7.tsx
@@ -1,10 +1,63 @@
 import React from 'react'
+import { Label } from './ui/label'
+import { Card, CardDescription, CardHeader, CardTitle } from './ui/card'
 
 const Section7 = () => {
   return (
-    <div>
-      Section 7
-    </div>
+    <section className="max-w-[1035px] bg-[#1b1c1d] text-[#f2e782ff] mt-28">
+      <div className='w-full h-full flex flex-col items-start'>
+        <div className='font-prata leading-tight mb-4 w-full'>
+
+          <Label className="text-[51px] block">
+            Leveraging Crypto Finance for Sustainable Impact
+          </Label>
+          {/* <Label className="text-[45px]"> Impact </Label> */}
+        </div>
+        <div className='flex items-center'>
+          <Card className="border-none bg-transparent">
+            <CardHeader className="text-center text-[#cfcbbf]">
+              <CardTitle className="text-[59.4px] font-prata">24/7</CardTitle>
+              <CardDescription className="text-[22.5px] font-prata">Always Open</CardDescription>
+              <CardDescription className="text-[18px] font-raleway">Trade carbon credits around the clock, unlike traditional markets.</CardDescription>
+            </CardHeader>
+
+          </Card>
+
+
+
+          <Card className="flex flex-col items-center border-none bg-transparent">
+            <CardHeader className="text-center text-[#cfcbbf]">
+              <CardTitle className="text-[59.4px] font-prata">0%</CardTitle>
+              <CardDescription className="text-[22.5px] font-prata">Zero Fees</CardDescription>
+              <CardDescription className="text-[18px] font-raleway">Enjoy fee-free trading for the first 6 months after launch.</CardDescription>
+            </CardHeader>
+          </Card>
+
+        </div>
+
+        <div className='flex items-center'>
+          <Card className="flex flex-col items-center border-none bg-transparent">
+            <CardHeader className="text-center text-[#cfcbbf]">
+              <CardTitle className="text-[59.4px] font-prata">100+</CardTitle>
+              <CardDescription className="text-[22.5px] font-prata">Cryptocurrencies</CardDescription>
+              <CardDescription className="text-[18px] font-raleway">Use a wide range of cryptocurrencies to purchase VCCs.</CardDescription>
+            </CardHeader>
+          </Card>
+
+
+          <Card className="flex flex-col items-center border-none bg-transparent">
+            <CardHeader className="text-center text-[#cfcbbf]">
+              <CardTitle className="text-[59.4px] font-prata">5min</CardTitle>
+              <CardDescription className="text-[22.5px] font-prata">Quick Setup</CardDescription>
+              <CardDescription className="text-[18px] font-raleway">Start trading in minutes with our streamlined onboarding process.</CardDescription>
+            </CardHeader>
+          </Card>
+
+        </div>
+
+      </div>
+
+    </section>
   )
 }
 
